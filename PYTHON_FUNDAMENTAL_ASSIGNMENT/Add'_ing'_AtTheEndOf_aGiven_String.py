b7a7bf2acde11e61c4a322a4330e897d1d23df21
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-11 16:17:07 +0530

    Update Add'_ing'_AtTheEndOf_aGiven_String.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/Add'_ing'_AtTheEndOf_aGiven_String.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/Add'_ing'_AtTheEndOf_aGiven_String.py
@@ -1,9 +1,16 @@
+'''
+    Write a Python program to add 'ing' at the end of a given string (lengthshould be at least 3).
+    If the given string already ends with 'ing' then add 'ly' instead
+    if the string length of the given string is less than 3, leave it unchanged.
+'''
 
-
+# Enter String
 string= input("Enter a String: ")
 
+
+# Condition 
 if len(string) > 3:
-    if string.endswith('ing'):
+    if string.endswith('ing'):   
         result= string + 'ly'
 
     else:
@@ -12,5 +19,5 @@ if len(string) > 3:
 else:
     result= string
 
-
+# Print The Result
 print("Result: ",result)
