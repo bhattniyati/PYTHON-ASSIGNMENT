970c9d2a844441ec39b9748d5e23d626f971b46d
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-11 16:19:43 +0530

    Update Calculate_LenOF_String.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/Calculate_LenOF_String.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/Calculate_LenOF_String.py
@@ -1,7 +1,9 @@
+'''
+  Write a Python program to calculate the length of a string.
+'''
 
-
-
+# Enter a Name
 name= input("Enter The Name: ")
 
-print(name)
-print("Len Of String is: ",len(name))
+print(name) # Print name
+print("Len Of String is: ",len(name))  # Chcek the Len of String
