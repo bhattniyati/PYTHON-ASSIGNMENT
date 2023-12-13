f87b27eb5fcc89ad941b13a6c9f702b56181753f
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-13 16:26:49 +0530

    Update TwoGivenStrings_SeparatedBySpace_&_SwapfirstTwoChar.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/TwoGivenStrings_SeparatedBySpace_&_SwapfirstTwoChar.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/TwoGivenStrings_SeparatedBySpace_&_SwapfirstTwoChar.py
@@ -1,14 +1,27 @@
+'''
+  Write a Python program to get a single string from two given strings,
+  separated by a space and swap the first two characters of each string.
 
+'''
 # Two Given Strings:
 
 string1= "English"
 string2= "Gujarati"
 
-new_string= string2[:2]+ string1[2:]+ ' ' + string1[:2]+string2[2:]
+'''
+  new_string = Guglish+ ' '+ Enjarati= Guglish Enjarati -- string2[:2] is store string2 of first two letter and 
+                                                           string1[2:] is store string1  of 2 index with last all letter
 
+'''
 
+new_string= string2[:2]+ string1[2:]+ ' ' + string1[:2]+string2[2:] 
+
+# Display the original strings
 print(f"Original Strings: \nString 1: {string1} \nString 2: {string2}")
+
 print("\nAfter Separated by Space & Swapping First Two Character: ")
+
+# Display new string
 print(new_string)
 
 
