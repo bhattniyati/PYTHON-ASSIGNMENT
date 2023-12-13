6b1b3d9c9e3cf3861ec8050bdab79b9de0933380
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-13 16:13:51 +0530

    Update Vowel_Or_Not.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/Vowel_Or_Not.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/Vowel_Or_Not.py
@@ -1,7 +1,19 @@
+'''
+    Write a Python program to test whether a passed letter is a vowel or not.
 
-vowel=['a','e','i','o','u']
+'''
 
+vowel=['a','e','i','o','u'] # store the vowel
+
+
+# Enter a alphabet
 letter=str(input("Enter Any Alphabet: "))
+
+'''
+    Chcek the condition if user enter a letter is in store vowel so print the msg letter is vowel,
+    but user enter a letter is not in store vowel so print the msg letter is not a vowel
+
+'''
 
 if letter in vowel:
     print("The Letter is a Vowel..")
