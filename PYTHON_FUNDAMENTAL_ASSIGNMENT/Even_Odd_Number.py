3c3d475e5d090d12ee634ce8472ade487d529775
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-13 15:53:11 +0530

    Update Even_Odd_Number.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/Even_Odd_Number.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/Even_Odd_Number.py
@@ -1,10 +1,14 @@
+'''
+    Write a Python program to find whether a given number is even or odd,
+    print out an appropriate message to the user.
 
+'''
 
-
+# Enter a number
 i= int(input("Enter The Number: "))
 
 if i%2==0:
-    print("Even Number")
+    print("Even Number")   # Chcek the condition if i%2==0 so print even number and not i%2==0 so print odd number
 
 else:
     print("Odd Number")
