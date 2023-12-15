a6c649d2d05543a5881967d62371b091b164f35b
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-15 16:23:21 +0530

    Update Swapping_With_Third_Variable.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/Swapping_With_Third_Variable.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/Swapping_With_Third_Variable.py
@@ -1,31 +1,42 @@
 
+# Swap two numbers with temp variable
 print("Swap Two Numbers With Temp Variable...")
 
+
+# a in 10 nd b in 20 store..
 A=10
 B=20
 
 print()
+
+# Print a and b before swap
 print("Before Swap Values Are: ","A=",A,"B=",B,)
 
-Temp=A
-A=B
-B=Temp
 
-print("After Swap Values Are: ","A=",A,"B=",B)
+Temp=A  # value of a store in temp variable
+A=B # value of b store in a variable
+B=Temp # value of temp store in b 
+
+# Print a and b after swap 
+print("After Swap Values Are: ","A=",A,"B=",B)  # swap two numbers with temp variable
 
 print()
 
+# Swap two numbers without temp variable
 print("Swap Two Numbers Without Temp Variable...")
 
 print()
 
+# Enter two numbers
 A=int(input("Enter First Number: "))
 B=int(input("Enter Second Number: "))
 
+# Print a and b befor swap 
 print("Before Swap Values Are: ","A=",A,"B=",B)
 
-A=A+B
+A=A+B 
 B=A-B
 A=A-B
 
+# Print a and b after swap
 print("After Swap Values Are: ","A=",A,"B=",B)
