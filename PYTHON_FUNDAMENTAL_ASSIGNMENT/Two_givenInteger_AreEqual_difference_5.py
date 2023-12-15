68422c65e2f50080641c42677fa8e6b0456b72f2
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-15 16:12:57 +0530

    Update Two_givenInteger_AreEqual_difference_5.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/Two_givenInteger_AreEqual_difference_5.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/Two_givenInteger_AreEqual_difference_5.py
@@ -1,14 +1,20 @@
+'''
+    Write a Python program that will return true if the two given integer values are equal or their sum or difference is 5.
+'''
 
 
+# Enter Two Numbers: 
 num1= int(input("Enter the First Number: "))
 num2= int(input("Enter The Second Number: "))
 
 
-if num1==num2 or num1+num2== 5 or abs(num1-num2)==5:
+# Condition num1 nd num2 equal nd difference is 5 so condition true else condition is false...
+if num1==num2 or num1+num2==5 or  num1-num2==5:
     result= True
 
 else:
     result=False
 
 
+# Display Result
 print(result)
