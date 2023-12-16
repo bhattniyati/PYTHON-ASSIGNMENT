8a3fa171ba7d875c3f63ad5a2a34e30ae6888c97
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-16 14:41:06 +0530

    Update Positive_Negative_Zero.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/Positive_Negative_Zero.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/Positive_Negative_Zero.py
@@ -1,7 +1,13 @@
+'''
+    Write a Python program to check if a number is positive, negative or zero.
+'''
 
-
-
+# Enter The Number..
 num=int(input("Enter The Number: "))
+
+'''
+    if num>0 so print number is positive, num<0 so print number is negative and two condition false so print number is zero..
+'''
 
 if num>0:
     print("The Number is Positive..")
