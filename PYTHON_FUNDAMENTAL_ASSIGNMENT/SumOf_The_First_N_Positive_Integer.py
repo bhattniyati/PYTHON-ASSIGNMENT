dcefa6cb81879c07246b2268aad5f67996bddc1c
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-16 14:38:28 +0530

    Update SumOf_The_First_N_Positive_Integer.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/SumOf_The_First_N_Positive_Integer.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/SumOf_The_First_N_Positive_Integer.py
@@ -1,9 +1,12 @@
+'''
+  Write a python program to sum of the first n positive integers.
+'''
 
-
-
+# Enter the number
 n= int(input("Enter The Number: "))
 
-sum= (n * (n+1)) / 2
+sum= (n * (n+1)) / 2   
 
 
+# Display Sum of Positive integers
 print("Sum Of The First",n, "Positive Integers: ",sum)
