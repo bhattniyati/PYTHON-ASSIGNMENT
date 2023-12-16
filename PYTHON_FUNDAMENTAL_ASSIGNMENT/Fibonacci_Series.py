3a65b47e2450d6c242c3f936496d704b2750dc74
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-16 15:00:47 +0530

    Update Fibonacci_Series.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/Fibonacci_Series.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/Fibonacci_Series.py
@@ -1,14 +1,23 @@
+'''
+    Write a Python program to get the Fibonacci series of given range.
+'''
 
-
-
+# declare 3 variable with value
 n1=0
 n2=1
 n3=2
-n3=n1+n2
+n3=n1+n2 # n3 store sum of n1, n2
 
+# Enter a steps
 end=int(input("Enter How Many Steps You Want? "))
 
+# print n1,n2,n3 and end= "" blank
 print("Fibonacci Series: ",n1,n2,n3,end=" ")
+
+'''
+    loop starting from 1 and end-2 in every number is '-' by 2
+    and print n3
+'''
 
 for i in range(1,end-2):
     n1=n2
