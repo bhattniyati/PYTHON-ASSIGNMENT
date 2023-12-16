8554abf5ba7cc8410bb7f78cdcdf5f97c83a719d
bhattniyati <135587128+bhattniyati@users.noreply.github.com>
2023-12-16 15:13:32 +0530

    Update Add'_ing'_AtTheEndOf_aGiven_String.py

--- PYTHON_FUNDAMENTAL_ASSIGNMENT/Add'_ing'_AtTheEndOf_aGiven_String.py
+++ PYTHON_FUNDAMENTAL_ASSIGNMENT/Add'_ing'_AtTheEndOf_aGiven_String.py
@@ -8,7 +8,12 @@
 string= input("Enter a String: ")
 
 
-# Condition 
+'''
+    Condition if string grater than 3 so second time check a condition if the string ends with ing so print result string + ly,
+    and condition false so print result string + ing as it is ,
+    and outer condition false so print result only string...
+'''
+
 if len(string) > 3:
     if string.endswith('ing'):   
         result= string + 'ly'
